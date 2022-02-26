003055997dae61b4f17f8a9296637924e25af05c
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2022-02-26 10:15:43 +0530

    main

--- Hello/settings.py
+++ Hello/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'e+e#vq+j1^$m+ipl$m6n$&fk!k!ewtkoqe$4--!#tfvx&j5w#&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','mywebsiteseven.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','mywebsiteseven.herokuapp.com','mlseven7.herokuapp.com']
 
 
 # Application definition
